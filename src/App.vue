9a3940beeaac9ed5f3edceb17c198d9a28347963
Ty Janik <tyjanik44@gmail.com>
2026-07-18 20:15:42 -0400

    Able to fetch standings data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from "axios";
+
+interface Team {
+    id: number;
+    link: string;
+    name: string;
+}
+
+interface TeamRecord {
+    team: Team;
+    wins: number;
+    losses: number;
+}
+
+interface Record {
+    teamRecords: TeamRecord[];
+}
+
+interface StandingsResponse {
+    records: Record[];
+}
+
+const fetchStandingsData = async () => {
+    const url = "https://statsapi.mlb.com/api/v1/standings?leagueId=103,104";
+
+    const response = await axios.get<StandingsResponse>(url);
+};
+
+fetchStandingsData();
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+    <h1>MLB Playoff Picture</h1>
 </template>
 
 <style scoped></style>
