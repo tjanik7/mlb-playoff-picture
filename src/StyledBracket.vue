b8d4820dca8e92ab5bb390604d9c871346de2c5f
Ty Janik <tyjanik44@gmail.com>
2026-07-26 13:44:12 -0400

    Init sandbox

--- src/StyledBracket.vue
+++ src/StyledBracket.vue
@@ -1,29 +1,51 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="container">
-        <div class="team">Cubs</div>
-        <div class="team bottom">Brewers</div>
+    <div class="row-container">
+        <div class="col-container">
+            <div class="flex-item">Cubs</div>
+            <div class="flex-item bottom">Dodgers</div>
+            <div class="flex-item">Padres</div>
+            <div class="flex-item bottom">Cardinals</div>
+        </div>
+        <div class="col-container">
+            <div class="flex-item">CHC</div>
+            <div class="flex-item bottom invisible">.</div>
+            <div class="flex-item bottom">SD</div>
+        </div>
     </div>
 </template>
 
 <style scoped lang="css">
-.container {
-    background-color: lightcoral;
-    display: grid;
-    grid-template-columns: min-content;
+.row-container {
+    background-color: lightgreen;
+
+    display: flex;
+    align-items: stretch;
 }
 
-.team {
-    /* width: fit-content; */
+.col-container {
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
     background-color: lightblue;
 
-    padding: 4px 16px;
+    justify-content: center;
+}
 
+.flex-item {
+    background-color: coral;
+    width: 100%;
     border-bottom: 3px solid black;
+    box-sizing: border-box;
+    /* flex-grow: 1; */
 }
 
 .bottom {
     border-right: 3px solid black;
+}
+
+.invisible {
+    border-bottom: none;
 }
 </style>
